9e36e793bfadadb17f532185942792c7579c2fa2
Dominik Moritz <domoritz@gmail.com>
2013-04-13 20:25:10 +0200

    Allow hashing std::pair

--- libclasp/clasp/pb_constraint.h
+++ libclasp/clasp/pb_constraint.h
@@ -26,10 +26,35 @@
 #endif
 
 #include <tr1/unordered_map>
+#include <algorithm>
 #include <clasp/constraint.h>
 #include <clasp/weight_constraint.h>
 #include <clasp/solver.h>
 #include <clasp/clause.h>
+
+// from boost functional
+template <class T>
+inline void hash_combine(std::size_t & seed, const T & v)
+{
+	std::tr1::hash<T> hasher;
+	seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+namespace std
+{
+namespace tr1
+{
+template<typename S, typename T> struct hash<pair<S, T> >
+{
+	inline size_t operator()(const pair<S, T> & v) const
+	{
+		size_t seed = 0;
+		::hash_combine(seed, v.first);
+		::hash_combine(seed, v.second);
+		return seed;
+	}
+};
+}} // namespace
 
 namespace Clasp {
 
